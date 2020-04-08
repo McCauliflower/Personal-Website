ebcea53241d0b7ed82db793b96459fb55854da58
mccauliflower <mccauliflower@protonmail.com>
2020-04-08 00:16:32 -0400

    remove consoles

--- src/utils/apiCalls.ts
+++ src/utils/apiCalls.ts
@@ -1,16 +1,13 @@
 import axios from "axios";
-import { env } from './helpers'
 
 export async function getImages() {
   const response = await axios.get(
-    env === "development" ? `${process.env.VUE_APP_BASE_URL}/getArtworks.json` : `${process.env.VUE_APP_BASE_URL}/server/artworks`
-  );
+    `https://electricAlchemy.io/server/artworks`);
   return response.data;
 }
 
 export async function getCodeProjects() {
   const response = await axios.get(
-    env === "development" ? `${process.env.VUE_APP_BASE_URL}/getCodeProjects.json` : `${process.env.VUE_APP_BASE_URL}/server/code`
-  );
+    `https://electricAlchemy.io/server/code`);
   return response.data;
 }
